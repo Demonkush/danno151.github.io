0a673d4e63066a7cbf6f9b1190229feb071b1369
Sarah-PC\Sarah <daniel.fewster@uqconnect.edu.au>
2017-12-28 12:51:28 -0500

    minor change

--- cinema/js/theater.js
+++ cinema/js/theater.js
@@ -408,7 +408,7 @@ function registerPlayer( type, object ) {
 	
 	var AnimeTwist = function() {
 		// Currently borrowed API until I get my own, for $60 :) kms dude
-		//jwplayer.key = "GBbtI9R8M4R2gQOTSs7m7AdoMdxpK3DD4IcgmQ==";
+		jwplayer.key = "GBbtI9R8M4R2gQOTSs7m7AdoMdxpK3DD4IcgmQ==";
 
 		var viewer = jwplayer("player");
 		viewer.setup({
